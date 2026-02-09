12e4141374dd3cb6e7aff3a2dcb46f9aa4ade655
Manasa Chittamanasareddy <chittamanasareddy@gmail.com>
2026-02-09 20:22:48 +0530

    Fixed merge conflicts and built AddTwoInts service server successfully

--- src/add_two_ints_server.cpp
+++ src/add_two_ints_server.cpp
@@ -3,6 +3,9 @@
 
 #include <memory>
 
+using std::placeholders::_1;
+using std::placeholders::_2;
+
 void add(
   const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
   std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
@@ -17,4 +20,25 @@ void add(
   RCLCPP_INFO(
     rclcpp::get_logger("add_two_ints_server"),
     "Sending response: sum=%ld",
-    response
+    response->sum);
+}
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto node = rclcpp::Node::make_shared("add_two_ints_server");
+
+  auto service = node->create_service<example_interfaces::srv::AddTwoInts>(
+    "add_two_ints",
+    &add);
+
+  RCLCPP_INFO(
+    rclcpp::get_logger("add_two_ints_server"),
+    "Add Two Ints Service Server started");
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
+  return 0;
+}
